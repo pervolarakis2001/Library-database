df7d2d2798c09dadc4ada183558e86a4f44bf446
pervolarakis2001 <132392804+pervolarakis2001@users.noreply.github.com>
2023-05-04 00:23:25 +0300

    Add files via upload

---
+++ Library/Website/route.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, abort
+from flask_mysqldb import MySQL
+from Website import app, db  # initially created by __init__.py, need to be used here
+
+
+@app.route("/")
+def index():
+    return render_template("landing.html", pageTitle="Landing Page")
+
+@app.route("/sign-up")
+def sign_up():
+    return render_template("sign_up.html", pageTitle="sign-up Page")
