bfda9a22c1ed16a71c353ce63a7811edc5b94c29
Pervolarakis Dimitrios <132392804+pervolarakis2001@users.noreply.github.com>
2023-05-06 15:05:32 +0300

    Add files via upload

--- Library/Website/route.py
+++ Library/Website/route.py
@@ -7,11 +7,15 @@ from Website import app, db  # initially created by __init__.py, need to be used
 def index():
     return render_template("landing.html", pageTitle="Landing Page")
 
-@app.route("/sign-up")
+@app.route("/sign-up",methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        firstName = request.form.get('Fist Name:')
+        lastName = request.form.get('Last Name:')
+    
     return render_template("sign_up.html")
 
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
     return render_template("login.html")
 
