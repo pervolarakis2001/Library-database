0306aa5c9a275f76132b2a59b34bdb6176713627
Pervolarakis Dimitrios <132392804+pervolarakis2001@users.noreply.github.com>
2023-05-31 22:11:17 +0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -25,12 +25,7 @@ WHERE schu.status = "teacher" AND borrowing_date > DATE_ADD(curdate(),interval -
 
 
 -- 3.1.3 
---select schu.school_users_id, us.First_name, us.Last_name, count(bor.borrowed_id) from books b
---INNER JOIN  borrowings bor ON bor.ISBN = b.ISBN
---INNER JOIN school_users schu ON bor.school_users_id = schu.school_users_id
---INNER JOIN users us ON us.user_id = schu.school_users_id
---WHERE year(CURDATE()) - DATE_FORMAT(schu.age,"%Y")  < 40 AND schu.status = "teacher";
--- exun daneistei ta perissotera vivlia, idk ti ennoei me ayto 
+
 
 SELECT subquery.First_name, subquery.Last_name, MAX(subquery.borrow_count) AS max_borrow_count
                 FROM (
@@ -46,15 +41,9 @@ SELECT subquery.First_name, subquery.Last_name, MAX(subquery.borrow_count) AS ma
                 GROUP BY subquery.First_name, subquery.Last_name; 
 
 -- 3.1.4
-SELECT DISTINCT aut.author FROM author_table aut
-LEFT JOIN borrowings bor ON bor.ISBN = aut.ISBN
-WHERE bor.borrowed_id is NULL;
--- mporei na lythei kai me subquery tha to dw argotera an ayti h lysi einai lathos
--- me subquery
 select distinct aut.author from author_table aut
-LEFT JOIN borrowings bor ON bor.ISBN = aut.ISBN
-WHERE not exists (select 1 from borrowings inner join author_table aut1 on bor.ISBN = aut1.ISBN );
-
+                    LEFT JOIN borrowings bor ON bor.ISBN = aut.ISBN
+                    WHERE not exists (select 1 from borrowings inner join author_table aut1 on bor.ISBN = aut1.ISBN );
 -- 3.1.5
 
 select bor.operator_id, us.First_name, us.Last_name,count(bor.borrowed_id) as count from users us
@@ -81,7 +70,6 @@ select ct1.category as cat1, ct2.category as cat2, count(bor.borrowed_id) as cou
 -- 3.1.7
 
     
--- answer the question
 with aut_five_less_max (author,count_of_books_per_author) as
 		(select aut.author, count(b.ISBN) as count_of_books_per_author from books b 
 		inner join author_table aut on aut.ISBN = b.ISBN 
@@ -103,26 +91,13 @@ select bor.school_users_id, us.First_name, us.Last_name from users us
 inner join borrowings bor on bor.school_users_id = us.user_id
 where due_date < curdate() and return_date is null
 having count(bor.borrowed_id) > 1;
--- thelei diorthwsi, einai lathos 
 
--- 3.2.3
-
---select school_users_id, avg(z.num_of_ratings_per_borrower) as avg_num_of_ratings_per_borrower from 
-	--(select school_users_id, count(rating_id) as num_of_ratings_per_borrower from ratings group by school_users_id) z
-   -- group by school_users_id;
-
-
---select category, avg(z.num_of_ratings_per_category) as avg_num_of_ratings_per_category from 
---	(select cat.category, count(r.rating_id) as num_of_ratings_per_category from ratings r inner join category_table cat on cat.ISBN = r.ISBN
-  --  group by cat.category) z
- --   group by category;
 
 select avg(rating_score) as score,school_users_id,category from ratings r INNER JOIN category_table c  on c.Isbn = r.ISBN WHERE 
              r.school_users_id = {sch_user_id} and c.category = '{category}'
 
 -- 3.3.1
-select * from reservations;
-select * from borrowings;
+
 -- 3.3.2
 select bor.school_users_id, us.First_name, us.Last_name, b.ISBN, b.title from borrowings bor 
 inner join books b on b.ISBN = bor.ISBN
