f94bd186da0049ab9205c48393923f0f7f157f84
Pervolarakis Dimitrios <132392804+pervolarakis2001@users.noreply.github.com>
2023-06-02 16:23:10 +0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,6 +1,6 @@
 
 -- 3.1.1 
- select sch.name as school, count(borrowed_id) as count from borrowings bor 
+select sch.name as school, count(borrowed_id) as count from borrowings bor 
                         INNER JOIN operator op ON bor.operator_id = op.operator_id 
                         INNER JOIN school sch ON  sch.operator_id = op.operator_id WHERE MONTH(borrowing_date) ='{month}' AND YEAR(borrowing_date) ='{year}'
                         GROUP BY sch.name;
